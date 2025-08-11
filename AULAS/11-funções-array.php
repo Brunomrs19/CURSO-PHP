43804f8bc7f6b288a9a51b8d0a3fbefd099a0abc
Brunomrs19 <brunomrs19@gmail.com>
2025-08-11 01:40:21 -0300

    Finalizado algumas funções de arrays

--- AULAS/11-funções-array.php
+++ AULAS/11-funções-array.php
@@ -8,23 +8,24 @@
 *is_array($array) = verifica se realmente é um array
 *in_array($valor, $array) = verifica se um determinado valor existe no array
 *arrya_keys($array) = retorna um novo array com as chaves virando valor
-*array_values($array) = retorna um novo array com os valores virando chaves
+*array_values($array) = reindexa o array numérico começando do índice 0.
 *array_merge($array1, $array2) = agrega o conteúdo dos dois arrays
 *array_pop($array) = exclui a ultima posição do array
+*array_push($array, "valor", "valor") = add um ou mais elementos no final do array
 *array_shift($array) = exclui a primeira posição do array
 *array_unshift($arr, "valor") = add um ou mais elementos no inicio do array
-*array_push($array, "valor", "valor") = add um ou mais elementos no final do array
-*array_combine($kets, $values) = mescla os dois arrays passados
+*array_combine($keys, $values) = mescla os dois arrays passados
 *array_sum() = calcula a soma dos elementos de um array
+
 *explode("/", "26/10/2001") = transforma string em array
 *implode("-", $arr) = transforam array em string
 */
 
-$cores = [
-    "verde", 
-    "azul",
-    "vermelho"
-];
+    $cores = [
+        "verde", 
+        "azul",
+        "vermelho"
+    ];
 
 print_r( $cores);
 
@@ -50,3 +51,110 @@ $keys = [
 print_r($keys);
 echo "<br>";
 print_r(array_keys($keys));
+echo "<hr>";
+
+
+//array values
+
+$nomes = [
+    "Iluminado" => "Gabi",
+    "Banco" => "itaú",
+    "Músico" => "Jota"
+];
+
+$nomesNum = array_values($nomes);
+print_r($nomesNum);
+echo "<hr>";
+
+
+//array merge
+$merge = array_merge($keys, $nomes);
+print_r($merge);
+echo "<hr>";
+
+
+//array pop
+array_pop($nomes);
+print_r($nomes);
+echo "<hr>";
+
+
+//array push
+array_push($nomes, "Jota"); //ou
+$nomes["Campeão"] = "Bask";
+print_r($nomes);
+echo "<hr>";
+
+
+
+//array shift
+
+array_shift($nomes);
+print_r($nomes);
+echo "<hr>";
+
+
+
+//array unshift
+array_unshift($nomes, "Gabi");
+print_r($nomes); 
+echo "<hr>";
+
+
+
+//array combine = numero desigual de keys ele retorna error!
+$rank = [
+    "S+",
+    "S",
+    "S-",
+    "A+"
+];
+
+$jogadores = [
+    "CR7",
+    "Neymar",
+    "Messi",
+    "Vini"
+];
+
+$result = array_combine($rank, $jogadores);
+print_r($result);
+echo "<hr>";
+
+
+
+//array sum
+$num = [
+    8,
+    15,
+    19,
+    12
+];
+print_r($num);
+echo "<br>";
+
+$somaNum = array_sum($num);
+print_r($somaNum);
+echo "<hr>";
+
+
+
+//explode ou preg_split("/.-\", $string)
+$data = "26/10/2001";
+$novaData = explode("/", $data);
+print_r($novaData);
+echo "<hr>";
+
+
+
+//implode
+$frase = "Boa noite bruno";
+
+//frase virou array, com separador space
+$array = explode(  " ", $frase);
+print_r($array);
+echo "<br>";
+
+//transformando em string
+$frase2 = implode(" ",$array);
+echo $frase2;
